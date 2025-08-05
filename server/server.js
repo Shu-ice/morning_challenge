44ab29dcc256830ada1cf98edca4267a28a13ce8
Shu-ice <moutaro@gmail.com>
2025-08-05 21:20:49 +0900

    fix: fsモジュールのインポート追加（Railway crash修正）

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import environmentConfig from './config/environment.js';
 import { logger } from './utils/logger.js';
